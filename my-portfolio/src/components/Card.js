7a00f5abac827cce4263f8417f2a0665d7ef6602
haylove <hfayelovelady@gmail.com>
2023-02-09 19:33:52 -0600

    components pages

--- my-portfolio/src/components/Card.js
+++ my-portfolio/src/components/Card.js
@@ -1,36 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 
 function Card(props) {
-    console.log(props.bingo.image) // ---> { bingo: { } }
-console.log(props.bingo)
-    const handleClick = (event) => {
-        console.log("Clicked");
+  console.log(props.bingo.image); // ---> { bingo: { } }
+  console.log(props.bingo);
+  const handleClick = (event) => {
+    console.log("Clicked");
 
-        console.log(event.target);
-        console.log(event.target.textContent);
-        let objData = event.target.textContent;
-        console.log(objData);
-        props.setSelected(objData);
+    console.log(event.target);
+    console.log(event.target.textContent);
+    let objData = event.target.textContent;
+    console.log(objData);
+    props.setSelected(objData);
+  };
 
-    }
+  return (
+    <div
+      className="card"
+      onClick={handleClick}
+      style={{
+        backgroundImage: `url(${props.bingo.image})`,
+        backgroundSize: "cover"
 
-    return (
-
-        <div 
-            className="card" 
-            onClick={handleClick}
-            style={{ backgroundImage: `url(${props.bingo.image})`,backgroundSize: 'cover'
-                 }}
-            >
-            <h1>{props.bingo.title}</h1>
-            <p className="tech">Tech Focus: {props.bingo.tech}</p>
-            <p className="area">Focus Area: {props.bingo.area}</p>
-            <img className="proj-img" src= {props.bingo.image} alt={props.bingo.title} />
-
-        </div>
-    );
+      }}
+    >
+      <h1>{props.bingo.title}</h1>
+      <p className="tech">Tech Focus: {props.bingo.tech}</p>
+      <p className="area">Focus Area: {props.bingo.area}</p>
+      <img
+        className="proj-img"
+        src={props.bingo.image}
+        alt={props.bingo.title}
+      />
+    </div>
+  );
 }
 
 export default Card;
